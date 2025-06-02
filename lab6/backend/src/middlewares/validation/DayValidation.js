f088722a96ee8c4e04bcf13c0d407b9653a1838c
IlliaSatyrenko <denzp05@gmail.com>
2025-06-02 20:10:35 +0300

    fixed validation

---
+++ lab6/backend/src/middlewares/validation/DayValidation.js
@@ -0,0 +1,40 @@
+import { body } from 'express-validator';
+
+export const createOrUpdateForecastValidation = [
+    body('location_id')
+        .notEmpty().withMessage('Location id is required')
+        .isInt().withMessage('Wrong location id format'),
+    body('date_id')
+        .notEmpty().withMessage('Date id is required')
+        .isInt().withMessage('Wrong date id format'),
+    body('hour')
+        .notEmpty().withMessage('Hour is required')
+        .isInt().withMessage('Wrong hour format'),
+    body('temperature')
+        .notEmpty().withMessage('Temperature is required')
+        .isInt().withMessage('Wrong temperature format'),
+    body('cloud_type')
+        .isIn(Object.values(ENUM('сонячно', 'частково хмарно', 'хмарно'))).withMessage('Wrong cloud type value')
+        .default('сонячно'),
+    body('wind_direction')
+        .isIn(Object.values(ENUM('Зх', 'Сх', 'Пн', 'Пд', 'ПнЗх', 'ПнСх', 'ПдЗх', 'ПдСх'))).withMessage('Wrong wind direction value')
+        .default('Пн'),
+    body('wind_speed')
+        .isInt().withMessage('Wrong wind speed format')
+        .default(0),
+    body('precipitation')
+        .isInt().withMessage('Wrong precipitation format')
+        .default(0),
+    body('is_rain')
+        .isBoolean().withMessage('Wrong rain format')
+        .default(false),
+    body('is_thunderstorm')
+        .isBoolean().withMessage('Wrong thunderstorm format')
+        .default(false),
+    body('is_snow')
+        .isBoolean().withMessage('Wrong snow format')
+        .default(false),
+    body('is_hail')
+        .isBoolean().withMessage('Wrong hail format')
+        .default(false),
+]
