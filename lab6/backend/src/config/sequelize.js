46bbe4e34192e1cd222bafb5e084d03fd4a5dfb0
ivan_yakovchuk <yakovchuk1005@gmail.com>
2025-06-02 22:59:49 +0300

    Path to dotenv fixed

--- lab6/backend/src/config/sequelize.js
+++ lab6/backend/src/config/sequelize.js
@@ -3,7 +3,7 @@ const path = require('path');
 const dotenv = require('dotenv');
 
 // Завантаження змінних середовища
-dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+dotenv.config({ path: path.resolve(__dirname, '../../../../.env') });
 
 const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
     host: 'localhost',
